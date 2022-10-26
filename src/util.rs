9acd8f9fd7fa1de968e0def5a8d600c6adb578c0
Wim de With <wf@dewith.io>
2022-10-26 14:46:54 +0200

    util: Add check_neg, check_zero and check_null macros
    Almost every function in libvirt follows the pattern of checking
    the return value for -1, 0 or NULL to detect an error.
    
    Signed-off-by: Wim de With <wf@dewith.io>
    [DB: annotations to hide warnings, rename check_int to
     check_neg and add check_zero]
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/util.rs
+++ src/util.rs
@@ -7,3 +7,46 @@ pub fn c_ulong_to_u64(val: ::libc::c_ulong) -> u64 {
 pub fn c_ulong_to_u64(val: ::libc::c_ulong) -> u64 {
     val as u64
 }
+
+#[allow(unused_macros)]
+macro_rules! check_null {
+    ($e:expr) => {{
+        let ptr = $e;
+        if ptr.is_null() {
+            Err($crate::error::Error::last_error())
+        } else {
+            Ok(ptr)
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! check_neg {
+    ($e:expr) => {{
+        let ret = $e;
+        if ret == -1 {
+            Err($crate::error::Error::last_error())
+        } else {
+            Ok(ret)
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! check_zero {
+    ($e:expr) => {{
+        let ret = $e;
+        if ret == 0 {
+            Err($crate::error::Error::last_error())
+        } else {
+            Ok(ret)
+        }
+    }};
+}
+
+#[allow(unused_imports)]
+pub(crate) use check_neg;
+#[allow(unused_imports)]
+pub(crate) use check_null;
+#[allow(unused_imports)]
+pub(crate) use check_zero;
