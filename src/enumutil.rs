a3b8490f18bba132d8ebe00139dfe2b4b8b8526d
Daniel P. Berrangé <berrange@redhat.com>
2024-08-14 18:06:58 +0100

    enumutil: add Enum class for wrapping rust enums
    Enum values received from libvirt are usually mapped to Rust enum
    constants. The libvirt driver that the app is talking to, however,
    might be returning values for which there is no Rust mapping yet.
    This is typical where new libvirt introduces new enum constants,
    but the app is using an older libvirt version, or simply because
    the latest Rust binding is not yet feature complete.
    
    When exposing enums in the API, it is important that we do not
    throw away any information that we received from libvirt, so we
    need a way to preserve unknown enum values.
    
    To achieve this, a new generic type is introduced that wraps the
    root Rust enum type:
    
      pub enum Enum<E, I> {
        Known(E),
        Unknown(I),
      }
    
    Consider a Rust enum
    
      enum ErrorLevel {
        None,
        Warning,
        Error,
      }
    
    which maps to the virErrorLevel C type, which may either be a
    plain integer type with CPP constants, or a real C enum.
    Either way, we can now define a Rust enum wrapper:
    
      pub type ErrorLevelEnum = Enum<ErrorLevel, sys::virErrorLevel>
    
    The Rust APIs / structs that are to be populated with enum values
    received from libvirt should all use the wrapper, ErrorLevelEnum.
    When mapping the libvirt value there are two options, either using
    a well known Rust enum identifier:
    
      let e: ErrorLevelEnum = Enum::Known<Warning>;
    
    Or using an unknown C value
    
      let e: ErrorLevelEnum = Enum::Unknown(42);
    
    Or if we don't know if the C value is known or unknown:
    
      let e: ErrorLevelEnum = from_raw(2);
    
    A caller that needs to be able to interpret the enum values has
    to decide their policy for unknown values.
    
    The lazy option is to panic by calling unwrap(), which is suitable
    if you are sure no unexpected values are present, or in throwaway
    tools not needing full error handling:
    
      let l: ErrorLevel = err.level().unwrap()
    
    A more intelligent option is to squash unknown values to a known
    value that will give good semantics. eg treat unknown error levels
    as being error
    
      let l: ErrorLevel = err.level().unwrap_or(ErrorLevel::Error)
    
    Finally the most explicit option is to manually match
    
      let l: ErrorLevel = match err.level() {
        Known(l) => ...
        Unknown(val) => ...
      };
    
    There are various other helper methods on the Enum type, inspired
    by similar methods on the Option and Result rust types.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/enumutil.rs
+++ src/enumutil.rs
@@ -1,6 +1,143 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
 pub trait RawEnum<I>: Sized {
     fn from_raw(raw: I) -> Option<Self>;
     fn to_raw(self) -> I;
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Enum<E, I> {
+    Known(E),
+    Unknown(I),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct UnknownEnumError<I> {
+    value: I,
+}
+
+impl<
+        E: std::fmt::Debug + std::cmp::PartialEq + std::marker::Copy + RawEnum<I>,
+        I: std::fmt::Display + std::marker::Copy,
+    > Enum<E, I>
+{
+    pub fn unwrap(self) -> E {
+        match self {
+            Enum::<E, I>::Known(k) => k,
+            Enum::<E, I>::Unknown(u) => {
+                panic!("unknown value {} in {}", u, std::any::type_name::<E>())
+            }
+        }
+    }
+
+    pub fn unwrap_or(self, def: E) -> E {
+        match self {
+            Enum::<E, I>::Known(k) => k,
+            Enum::<E, I>::Unknown(_) => def,
+        }
+    }
+
+    pub fn is_known(&self) -> bool {
+        match self {
+            Enum::<E, I>::Known(_) => true,
+            Enum::<E, I>::Unknown(_) => false,
+        }
+    }
+
+    pub fn is_known_and(self, f: impl FnOnce(E) -> bool) -> bool {
+        match self {
+            Enum::<E, I>::Known(k) => f(k),
+            Enum::<E, I>::Unknown(_) => false,
+        }
+    }
+
+    pub fn is_unknown(&self) -> bool {
+        match self {
+            Enum::<E, I>::Known(_) => false,
+            Enum::<E, I>::Unknown(_) => true,
+        }
+    }
+
+    pub fn is_unknown_and(self, f: impl FnOnce(I) -> bool) -> bool {
+        match self {
+            Enum::<E, I>::Known(_) => false,
+            Enum::<E, I>::Unknown(u) => f(u),
+        }
+    }
+
+    pub fn is(&self, want: E) -> bool {
+        match self {
+            Enum::<E, I>::Known(k) => want == *k,
+            Enum::<E, I>::Unknown(_) => false,
+        }
+    }
+
+    pub fn is_any(&self, want: Vec<E>) -> bool {
+        match self {
+            Enum::<E, I>::Known(k) => want.contains(k),
+            Enum::<E, I>::Unknown(_) => false,
+        }
+    }
+
+    pub fn known(self) -> Option<E> {
+        match self {
+            Enum::<E, I>::Known(k) => Some(k),
+            Enum::<E, I>::Unknown(_) => None,
+        }
+    }
+
+    pub fn unknown(self) -> Option<I> {
+        match self {
+            Enum::<E, I>::Known(_) => None,
+            Enum::<E, I>::Unknown(u) => Some(u),
+        }
+    }
+
+    /// Converts libvirt C enum constant to Rust enum.
+    pub fn from_raw(raw: I) -> Enum<E, I> {
+        match E::from_raw(raw) {
+            Some(e) => Enum::<E, I>::Known(e),
+            None => Enum::<E, I>::Unknown(raw),
+        }
+    }
+
+    /// Converts Rust enum to libvirt C enum constant.
+    pub fn to_raw(self) -> I {
+        match self {
+            Enum::Known(e) => e.to_raw(),
+            Enum::Unknown(v) => v,
+        }
+    }
+}
+
+impl<I> UnknownEnumError<I> {
+    pub fn value(self) -> I {
+        self.value
+    }
+}
+
+impl<E, I> From<E> for Enum<E, I> {
+    fn from(v: E) -> Enum<E, I> {
+        Enum::<E, I>::Known(v)
+    }
+}
+
+impl<E, I> From<Enum<E, I>> for Result<E, UnknownEnumError<I>> {
+    fn from(e: Enum<E, I>) -> Self {
+        match e {
+            Enum::<E, I>::Known(e) => Ok(e),
+            Enum::<E, I>::Unknown(v) => Err(UnknownEnumError::<I> { value: v }),
+        }
+    }
+}
+
+impl<E: std::fmt::Display, I: std::fmt::Debug + std::fmt::Display> Display for Enum<E, I> {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            Enum::<E, I>::Known(e) => write!(f, "{}", e),
+            Enum::<E, I>::Unknown(v) => write!(f, "{}({})", std::any::type_name::<E>(), v,),
+        }
+    }
 }
 
 macro_rules! impl_enum {
@@ -90,6 +227,8 @@ mod tests {
         Baz,
     }
 
+    type ExampleEnum = Enum<Example, u32>;
+
     impl_enum! {
         enum: Example,
         raw: u32,
@@ -126,5 +265,84 @@ mod tests {
             assert_eq!(variant.to_raw(), expected);
             assert_eq!(variant.to_string(), estr);
         }
+    }
+
+    #[test]
+    fn test_enum_known() {
+        let e = ExampleEnum::from_raw(BAR);
+
+        assert_eq!(e.to_raw(), BAR);
+        assert!(e.is_known());
+        assert!(!e.is_unknown());
+
+        assert!(e.is_known_and(|k| k == Example::Bar));
+        assert!(!e.is_known_and(|k| k == Example::Foo));
+        assert!(!e.is_unknown_and(|_| true));
+
+        assert!(e.is(Example::Bar));
+        assert!(!e.is(Example::Foo));
+        assert!(e.is_any(vec![Example::Foo, Example::Bar]));
+        assert!(!e.is_any(vec![Example::Foo, Example::Baz]));
+
+        assert_eq!(e.known(), Some(Example::Bar));
+        assert_eq!(e.unknown(), None);
+
+        assert_eq!(e.unwrap_or(Example::Foo), Example::Bar);
+        assert_eq!(e.unwrap(), Example::Bar);
+
+        assert_eq!(e.to_string(), "bar");
+    }
+
+    #[test]
+    fn test_enum_result_known() {
+        let e = ExampleEnum::from_raw(BAR);
+        let r: Result<Example, UnknownEnumError<u32>> = e.into();
+
+        assert!(r.is_ok());
+
+        assert_eq!(r.ok(), Some(Example::Bar));
+        assert_eq!(r.err(), None);
+
+        assert_eq!(r.unwrap_or(Example::Foo), Example::Bar);
+        assert_eq!(r.unwrap(), Example::Bar);
+    }
+
+    #[test]
+    fn test_enum_unknown() {
+        let e = ExampleEnum::from_raw(8);
+
+        assert_eq!(e.to_raw(), 8);
+        assert!(!e.is_known());
+        assert!(e.is_unknown());
+
+        assert!(!e.is_known_and(|_| true));
+        assert!(e.is_unknown_and(|u| u == 8));
+        assert!(!e.is_unknown_and(|u| u == 9));
+
+        assert!(!e.is(Example::Bar));
+        assert!(!e.is_any(vec![Example::Foo, Example::Bar]));
+
+        assert_eq!(e.known(), None);
+        assert_eq!(e.unknown(), Some(8));
+
+        assert_eq!(e.unwrap_or(Example::Foo), Example::Foo);
+        // type_name() output format is not guaranteed stable,
+        // so merely check that our unqualified typename is contained
+        // along with our value suffix
+        assert!(e.to_string().contains("Example"));
+        assert!(e.to_string().contains("(8)"));
+    }
+
+    #[test]
+    fn test_enum_result_unknown() {
+        let e = ExampleEnum::from_raw(8);
+        let r: Result<Example, UnknownEnumError<u32>> = e.into();
+
+        assert!(r.is_err());
+
+        assert_eq!(r.ok(), None);
+        assert_eq!(r.err(), Some(UnknownEnumError { value: 8 }));
+
+        assert_eq!(r.unwrap_or(Example::Foo), Example::Foo);
     }
 }
